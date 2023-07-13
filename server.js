606e09101720e100a389a0b0aff3d74a65e10dd9
Boris Fernando Calderon Sanchez <the_pross_bpd@hotmail.com>
2023-07-13 16:53:23 -0400

    commit 4

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require('express');
+
+const cors = require('cors');
+
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+
+const allowedOrigins = [
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:8100',
+  ];
+
+  // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+};
+// Enable preflight requests for all routes
+app.options('*', cors(corsOptions));
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 5000;
 app.listen(port);
